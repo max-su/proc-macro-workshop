2b0edf03d68cf27ceb386a7ea0c5e144a2e286ca
Max Su <msu@digitalocean.com>
2019-09-26 18:40:54 -0400

    builder-3

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use proc_macro2;
 use quote::{quote, format_ident};
-use syn::{parse_macro_input, DeriveInput, Data, Fields};
+use syn::{parse_macro_input, DeriveInput, Data, Ident, Fields};
 
 // Creates the struct definition fields
 fn builder_definition_fields(data: &Data) -> proc_macro2::TokenStream {
@@ -43,6 +43,28 @@ fn new_builder_fields(data: &Data) -> proc_macro2::TokenStream {
     }
 }
 
+fn builder_setter_methods(data: &Data) -> proc_macro2::TokenStream {
+    match *data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    let field_names = fields.named.iter().map(|f| &f.ident);
+                    let field_types = fields.named.iter().map(|f| &f.ty);
+                    quote! {
+                        #(
+                            pub fn #field_names(&mut self, #field_names: #field_types) -> &mut Self {
+                                self.#field_names = Some(#field_names);
+                                self
+                            }
+                        )*
+                    }
+                }, Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
+            }
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    }
+}
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input: DeriveInput = parse_macro_input!(input as DeriveInput);
@@ -51,8 +73,10 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let name = input.ident;
     // Used in the quasi-quotation for #builder_name
     let builder_name = format_ident!("{}Builder", name);
+
     let new_builder_fields = new_builder_fields(&input.data);
     let builder_definition_fields = builder_definition_fields(&input.data);
+    let setter_methods = builder_setter_methods(&input.data);
 
     let tokens = quote! {
         impl #name {
@@ -64,6 +88,9 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
         pub struct #builder_name {
             #builder_definition_fields
+        }
+        impl #builder_name {
+            #setter_methods
         }
     };
     eprintln!("TOKENS: {}", tokens.clone());
