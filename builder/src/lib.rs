6a9de543954e7a465e962d2f4ed53cec4bb1cc90
Max Su <msu@digitalocean.com>
2019-09-27 15:13:14 -0400

    builder-4

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -65,6 +65,35 @@ fn builder_setter_methods(data: &Data) -> proc_macro2::TokenStream {
     }
 }
 
+fn builder_method(data: &Data, name: &Ident) -> proc_macro2::TokenStream {
+    match *data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    let field_names = fields.named.iter().map(|f| &f.ident);
+                    let field_names_0 = field_names.clone();
+                    quote! {
+                        pub fn build(&self) -> Result<#name, Box<dyn Error>> {
+                            #(
+                            if (self.#field_names_0.is_none()) {
+                                return Err("One value is not initialized".into());
+                            }
+                            )*
+                            Ok(#name {
+                                #(
+                                    #field_names: self.#field_names.clone().unwrap(),
+                                )*
+                            })
+                        }
+                    }
+                }, Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
+            }
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    }
+
+}
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input: DeriveInput = parse_macro_input!(input as DeriveInput);
@@ -77,8 +106,10 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let new_builder_fields = new_builder_fields(&input.data);
     let builder_definition_fields = builder_definition_fields(&input.data);
     let setter_methods = builder_setter_methods(&input.data);
+    let builder_method = builder_method(&input.data, &name);
 
     let tokens = quote! {
+        use std::error::Error;
         impl #name {
             pub fn builder() -> #builder_name {
                 #builder_name {
@@ -91,6 +122,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
         impl #builder_name {
             #setter_methods
+            #builder_method
         }
     };
     eprintln!("TOKENS: {}", tokens.clone());
